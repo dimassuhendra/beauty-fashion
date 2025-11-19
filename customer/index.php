9ccf058574dc82205b8096f019648c7de6ebde30
Dimas Suhendra <dimassuhendra0104@gmail.com>
2025-11-19 16:05:42 +0700

    update footer index customer

--- customer/index.php
+++ customer/index.php
@@ -94,28 +94,7 @@ include 'proses/get_index.php';
         </div>
     </main>
 
-    <footer id="contacts-us" class="footer-hero text-white pt-5 pb-3">
-        <div class="container">
-            <h4 class="text-center mb-4 fw-bold">Kontak Kami</h4>
-            <div class="row text-center">
-                <div class="col-md-4 mb-3">
-                    <p class="lead mb-1">Email</p>
-                    <p class="fw-light">beautyfashionlampung@gmail.com</p>
-                </div>
-                <div class="col-md-4 mb-3">
-                    <p class="lead mb-1">Telepon</p>
-                    <p class="fw-light">+62 823-0601-7068</p>
-                </div>
-                <div class="col-md-4 mb-3">
-                    <p class="lead mb-1">Alamat Office</p>
-                    <p class="fw-light">Purwodadi Dalam Tanjung Sari Lampung Selatan</p>
-                </div>
-            </div>
-            <div class="text-center mt-4 pt-3 border-top border-light border-opacity-25">
-                <p>&copy; 2025 Beauty Fashion. All Rights Reserved.</p>
-            </div>
-        </div>
-    </footer>
+    <?php include '../footer.php' ?>
 
 
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"></script>
